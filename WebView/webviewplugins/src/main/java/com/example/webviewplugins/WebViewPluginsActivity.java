fba0d3fd7d0a7aa4d4a196146c90626d49912c31
54763755@qq.com <54763755@qq.com>
2021-07-05 17:20:56 +0800

    1.WebView项目，已经可以在模拟器上打开网易网页了。 2.Unity项目，已经可以调用jar包与android层交互了。 3.解决了gradle编译报dex错的问题。 4.解决了unity访问android代码的方法时报non-static method错误的问题。 5.下一步实现android library调用app中的activity的功能。

---
+++ WebView/webviewplugins/src/main/java/com/example/webviewplugins/WebViewPluginsActivity.java
@@ -0,0 +1,32 @@
+package com.example.webviewplugins;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.unity3d.player.UnityPlayer;
+import com.unity3d.player.UnityPlayerActivity;
+
+public class WebViewPluginsActivity extends UnityPlayerActivity {
+    public static WebViewPluginsActivity MyActivity ;  //自身引用
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        MyActivity = this;
+    }
+
+    //unity调用Android
+    public void UnityCallAndroid () {
+       Toast.makeText(this,"<><UnityCallAndroid>unity调用android成功", Toast.LENGTH_LONG).show();
+        AndroidCallUnity();
+   }
+
+    //android调用unity
+    public void AndroidCallUnity () {
+        //第1个参数为Unity场景中用于接收android消息的对象名称
+        //第2个参数为对象上的脚本的一个成员方法名称（脚本名称不限制）
+        //第3个参数为unity方法的参数
+        Toast.makeText(this,"<><UnityCallAndroid>android正在尝试调用unity", Toast.LENGTH_LONG).show();
+        UnityPlayer.UnitySendMessage("WebViewPlugins","AndroidCallback","That all will be ok!");
+
+  }
+}
